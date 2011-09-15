4592f4ca08aff7e4cedff3b0580afcd796b52146
Noah Gibbs <noah@ooyala.com>
2011-09-15 14:17:03 -0700

    Test inherited ecologies better

--- test/override_properties_test.rb
+++ test/override_properties_test.rb
@@ -7,13 +7,37 @@ class OverridePropertiesTest < Scope::TestCase
 
   context "with environment-from in your ecology" do
     setup do
+      set_up_ecology <<ECOLOGY_GRANDPARENT1_CONTENTS, "grandparent1.ecology"
+{
+  "testing": {
+    "capabilities": [ "rails", "rvm" ],
+    "othertag": 9
+  },
+  "monitoring": {
+    "property9": 71
+  }
+}
+ECOLOGY_GRANDPARENT1_CONTENTS
+
+      set_up_ecology <<ECOLOGY_GRANDPARENT2_CONTENTS, "grandparent2.ecology"
+{
+  "monitoring": {
+    "property3": 7,
+    "property9": 134
+  }
+}
+ECOLOGY_GRANDPARENT2_CONTENTS
+
       set_up_ecology <<ECOLOGY_PARENT_CONTENTS, "parent.ecology"
 {
+  "uses": ["grandparent1.ecology", "grandparent2.ecology"],
   "logging": {
     "property1": "foo"
   },
   "monitoring": {
-    "property2": "bar"
+    "property1": "burgers",
+    "property2": "bar",
+    "property3": "quux"
   }
 }
 ECOLOGY_PARENT_CONTENTS
@@ -27,16 +51,27 @@ ECOLOGY_PARENT_CONTENTS
   }
 }
 ECOLOGY_CONTENTS
+      Ecology.read
     end
 
     should "get overridden properties correctly" do
-      Ecology.read
       assert_equal "baz", Ecology.property("monitoring::property2")
     end
 
     should "get inherited properties correctly" do
-      Ecology.read
       assert_equal "foo", Ecology.property("logging::property1")
+    end
+
+    should "get properties in an overridden hash" do
+      assert_equal "burgers", Ecology.property("monitoring::property1")
+    end
+
+    should "get grandparent properties" do
+      assert_equal 9, Ecology.property("testing::othertag")
+    end
+
+    should "have first parent override second parent properties" do
+      assert_equal 71, Ecology.property("monitoring::property9")
     end
   end
 end
